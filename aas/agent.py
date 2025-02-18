Added normalization from MuZero and fixed multiprocessing bug

aas/agent.py
@@ -4,8 +4,7 @@ from aas.wrappers import AASNetworkWrapper, AASNetworkPolicyEstimation, AASNetwo
 from aas.node import Node
 from aas.mcts import MCTS
 from aas.state import OperationState
-from typing import Optional, Callable, Literal
-import multiprocessing.managers
+from typing import Optional, Callable, Literal, Iterable
 from copy import deepcopy
 import numpy as np
 import torch
@@ -57,9 +56,9 @@ class AlphaAutoScheduler:
             target_policy_estimation, next_node = mcts.run(node, n_iterations=cfg.mcts_nb_iterations, mode=mode)
             # max_q_child = max([child for child in node.children], key=lambda x: x.q, default=None)
             # max_nb_visits_child = max([child for child in node.children], key=lambda x: x.nb_visits, default=None)
-            # print(node.state.operation_tag, "Max Q", max_q_child.to_str(mcts.c_puct) if max_q_child is not None else None)
-            # print(node.state.operation_tag, "Fisrt child", node.children[0].to_str(mcts.c_puct) if len(node.children) > 0 else None)
-            # print(node.state.operation_tag, "Max nb visits", max_nb_visits_child.to_str(mcts.c_puct))
+            # print(node.state.operation_tag, "Max Q", max_q_child.to_str(mcts.min_value, mcts.max_value, mcts.c_puct) if max_q_child is not None else None)
+            # print(node.state.operation_tag, "Fisrt child", node.children[0].to_str(mcts.min_value, mcts.max_value, mcts.c_puct) if len(node.children) > 0 else None)
+            # print(node.state.operation_tag, "Max nb visits", max_nb_visits_child.to_str(mcts.min_value, mcts.max_value, mcts.c_puct))
             # Save the current state and the target policy estimation and set value to 0 for now
             trajectory.append((node.state, target_policy_estimation))
             # Make the next node the root node
@@ -71,23 +70,22 @@ class AlphaAutoScheduler:
         # Return the trajectory
         return trajectory
 
-    def run_parallel(self, state: OperationState, process_id: int, trajectories_list: multiprocessing.managers.ListProxy, mode: Literal['greedy', 'stochastic'] = 'stochastic'):
-        """Run the Alpha AutoScheduler on a given state and return training data about the trajectory taken by the agent
-        and put in a multiprocessing queue.
+    def run_parallel(self, args: Iterable):
+        """Run the Alpha AutoScheduler on a given state and return training data about the trajectory. This ùethod should be used
+        instead of the original one when doing multiprocessing.
 
         Args:
-            state (OperationState): The initial operation state to optimize.
-            process_id (int): The process id of the current process.
-            trajectories_list (multiprocessing.managers.ListProxy): The list to put the trajectory in.
-            mode (Literal['greedy', 'stochastic'], optional): The mode to run the agent. Defaults to 'stochastic'.
+            args (Iterable): The arguments to pass to the run method.
+
+        Returns:
+            list[tuple[OperationState, AASNetworkPolicyEstimation]]: The trajectory taken by the agent.
         """
         # Reseed numpy random generator
         np.random.seed()
         # Reseed torch random generator
         torch.manual_seed(np.random.randint(0, 2**32 - 1))
         # Run agent
-        trajectory = self.run(deepcopy(state), mode=mode)
-        trajectories_list.append((process_id, trajectory))
+        return self.run(*args)
 
     def train(self, data: list[tuple[OperationState, AASNetworkEstimation]]):
         """Train the Alpha AutoScheduler on given history data.

aas/env.py
@@ -2,7 +2,7 @@ from aas import config as cfg
 from aas.observation.benchmark import BenchmarkFeatures, extract_bench_features_from_file, extract_bench_features_from_code
 from aas.state import OperationState
 from aas.agent import AlphaAutoScheduler
-from aas.wrappers import AASNetworkPolicyEstimation, AASNetworkEstimation
+from aas.wrappers import AASNetworkEstimation
 from aas.evaluation import evaluate_code_with_timeout, evaluate_benchmark_code_with_timeout
 from typing import Optional, Literal
 import random
@@ -16,6 +16,7 @@ from utils.log import print_info, print_alert, print_success, print_error
 import neptune
 import torch
 import multiprocessing
+import time
 
 
 class AASOpEnv:
@@ -169,10 +170,8 @@ class AASOpEnv:
             float: The reward for the agent.
         """
         root_exec_time = state.bench_features.root_exec_time
-        speedup = root_exec_time / exec_time
-        return max(-3.0, min(3.0, math.log2(speedup) / 2 - 1.0))
-        # best_speedup = max(2.0, bench_features.best_speedup[state.operation_tag])
-        # return max(-1.0, min(1.0, math.log2(speedup / best_speedup)))
+        speedup = root_exec_time / exec_time if exec_time > 0 and root_exec_time > 0 else 1.0
+        return math.log2(speedup)
 
     def compare(self, agent1: AlphaAutoScheduler, agent2: AlphaAutoScheduler):
         """Compare two agents on a set of benchmarks.
@@ -188,53 +187,39 @@ class AASOpEnv:
         """
         # Run specified number of full benchmark episodes
         score = 0
-        speedups1 = []
-        speedups2 = []
-        manager = multiprocessing.Manager()
-        processes: list[multiprocessing.Process] = []
+        speedups1: list[float] = []
+        speedups2: list[float] = []
+        ins: list[tuple[OperationState, str]] = []
         terminated_list: list[bool] = []
-        trajectories_list = manager.list()
-        cpt = 0
-        # Set number of torch threads to 1 to avoid issues with multiprocessing
-        original_num_threads = torch.get_num_threads()
-        torch.set_num_threads(1)
+        mcts_start_time = time.time()
+        # Gather states
         for _ in range(cfg.nb_eval_eps):
-            last_states1: list[OperationState] = []
-            last_states2: list[OperationState] = []
             terminated = False
             state = self.reset()
             bench_features = state.bench_features
             while not terminated:
-                # Run agent1 on the current state
-                process1 = multiprocessing.Process(target=agent1.run_parallel, args=(state, cpt, trajectories_list, 'greedy'))
-                processes.append(process1)
-                process1.start()
-                cpt += 1
-                # Run agent2 on the current state
-                process2 = multiprocessing.Process(target=agent2.run_parallel, args=(state, cpt, trajectories_list, 'greedy'))
-                processes.append(process2)
-                process2.start()
+                # Add the current state to the list of states
+                ins.append((state, 'greedy'))
                 # Take a step in the environment
                 state, terminated = self.step(state)
                 terminated_list.append(terminated)
-                cpt += 1
-        for i in tqdm(range(len(processes)), desc="Eval MCTS search"):
-            # Wait for trajectory data in queue
-            processes[i].join()
-            processes[i].close()
+        # Set number of torch threads to 1 to avoid issues with multiprocessing
+        original_num_threads = torch.get_num_threads()
+        torch.set_num_threads(1)
+        with multiprocessing.Pool() as pool:
+            # Run the agent1 on the current states
+            trajectories1 = pool.map(agent1.run_parallel, ins)
+        with multiprocessing.Pool() as pool:
+            # Run the agent2 on the current states
+            trajectories2 = pool.map(agent2.run_parallel, ins)
         # Reset the number of torch threads
         torch.set_num_threads(original_num_threads)
-        # Sort trajectories
-        trajectories: list[tuple[int, tuple[OperationState, AASNetworkPolicyEstimation]]] = list(sorted(list(trajectories_list), key=lambda x: x[0]))
         # Get last states
         bench_states1 = []
         bench_states2 = []
         tmp_states1 = []
         tmp_states2 = []
-        for i in range(0, len(trajectories), 2):
-            trajectory1 = trajectories[i][1]
-            trajectory2 = trajectories[i + 1][1]
-            terminated = terminated_list[i // 2]
+        for trajectory1, trajectory2, terminated in zip(trajectories1, trajectories2, terminated_list):
             tmp_states1.append(trajectory1[-1][0])
             tmp_states2.append(trajectory2[-1][0])
             if terminated:
@@ -242,6 +227,9 @@ class AASOpEnv:
                 bench_states2.append(tmp_states2)
                 tmp_states1 = []
                 tmp_states2 = []
+        # Get MCTS search time
+        mcts_end_time = time.time()
+        print_info(f"MCTS Search ended in {mcts_end_time - mcts_start_time} s ...")
         # Execute optimized operations
         for i in tqdm(range(len(bench_states1)), desc="Evaluation execution"):
             last_states1 = bench_states1[i]
@@ -344,32 +332,27 @@ class AASTrainer:
         # Loop trough iterations
         for _ in tqdm(range(cfg.nb_iterations), desc="Main Loop"):
             # ======================== Gather training data ========================
-            # Get training states
-            manager = multiprocessing.Manager()
-            trajectories_list = manager.list()
-            processes: list[multiprocessing.Process] = []
             print_info("Started MCTS search ...")
+            mcts_start_time = time.time()
+            # Get training states
+            ins: list[tuple[OperationState]] = []
+            for _ in range(cfg.nb_train_eps):
+                # Add the current state to the training states
+                ins.append((state,))
+                # Take a step in the environment
+                state, _ = self.train_env.step(state)
             # Set number of torch threads to 1 to avoid issues with multiprocessing
             original_num_threads = torch.get_num_threads()
             torch.set_num_threads(1)
-            for j in range(cfg.nb_train_eps):
+            with multiprocessing.Pool() as pool:
                 # Run the agent on the current state
-                process = multiprocessing.Process(target=self.agent.run_parallel, args=(state, j, trajectories_list))
-                processes.append(process)
-                process.start()
-                # Take a step in the environment
-                state, _ = self.train_env.step(state)
-            for j in tqdm(range(len(processes)), desc="MCTS Search"):
-                # Wait for trajectory data in queue
-                processes[j].join()
-                processes[j].close()
-            # Save data gathered per iteration
-            trajectories: list[list[tuple[OperationState, AASNetworkPolicyEstimation]]] = [trajectory for _, trajectory in sorted(list(trajectories_list), key=lambda x: x[0])]
+                trajectories = pool.map(self.agent.run_parallel, ins)
             # Reset the number of torch threads
             torch.set_num_threads(original_num_threads)
+            mcts_end_time = time.time()
             print_info("Number of trajectories:", len(trajectories))
             print_info("Total number of data points:", sum(len(trajectory) for trajectory in trajectories))
-            print_info("MCTS search ended ...")
+            print_info(f"MCTS search ended in {mcts_end_time - mcts_start_time} s ...")
 
             # ======================== Execute trajectories ========================
             print_info("Started execution ...")

aas/evaluation.py
@@ -71,7 +71,7 @@ def evaluate_code_with_bindings(code: str, function_name: str) -> tuple[Optional
     with open(full_function_name, "r") as f:
         original_code = f.read()
 
-    np_file = np.load(full_function_name + ".npz")
+    np_file: np.lib.npyio.NpzFile = np.load(full_function_name + ".npz")
     expected: np.ndarray = np.load(full_function_name + ".npy")
 
     args_names: list[str] = sorted(
@@ -92,12 +92,14 @@ def evaluate_code_with_bindings(code: str, function_name: str) -> tuple[Optional
         execution_engine.invoke("main", *args)
         execution_engine.invoke("main", *args)
     except Exception as e:
+        np_file.close()
         return None, e
     actual = args_map[args_names[-1]]
     if expected.dtype == np.complex128:
         actual = actual.view(np.complex128).squeeze(len(actual.shape) - 1)
     assertion = np.allclose(actual, expected)
 
+    np_file.close()
     return delta_arg[0], assertion
 
 

aas/mcts.py
@@ -5,6 +5,7 @@ from aas.state import OperationState
 from aas.evaluation import get_cached_exec_time
 import numpy as np
 from typing import Callable, Literal
+import math
 
 
 class MCTS:
@@ -31,6 +32,8 @@ class MCTS:
         self.c_puct = cfg.mcts_c_puct
         self.action_temperature = 1.0
         self.action_temperature_decay = cfg.mcts_action_temperature_decay
+        self.max_value = -math.inf
+        self.min_value = math.inf
 
     def reset(self):
         """Reset the MCTS algorithm."""
@@ -50,7 +53,7 @@ class MCTS:
             # Get dirichlet noise for the children
             noises = np.random.dirichlet([0.03] * len(node.children))
             # Calculate S scores
-            scores = np.array([child.get_s_score(self.c_puct, noise=noises[i].item()) for i, child in enumerate(node.children)])
+            scores = np.array([child.get_s_score(self.min_value, self.max_value, self.c_puct, noise=noises[i].item()) for i, child in enumerate(node.children)])
             # Get the child node with the highest S score with random tie breaking
             node_id = np.random.choice(np.flatnonzero(scores == scores.max())).item()
             node = node.children[node_id]
@@ -89,6 +92,8 @@ class MCTS:
                 node_value = self.aas_network_wrapper.eval_node_value(child)
             # Update the node with its value
             child.update_leaf(node_value)
+            self.min_value = min(self.min_value, node_value)
+            self.max_value = max(self.max_value, node_value)
 
     def backpropagate(self, node: Node):
         """Backpropagate the speedup value up the MCTS tree.
@@ -119,6 +124,9 @@ class MCTS:
             AASNetworkPolicyEstimation: The AASNetwork policy estimation.
             Node: The child node with the highest MCTS probability. The root is returned if no children.
         """
+        # Update min and max values
+        self.min_value = min(self.min_value, root.q)
+        self.max_value = max(self.max_value, root.q)
         # Run the MCTS search for a given number of iterations
         for _ in range(n_iterations):
             node = self.select(root)